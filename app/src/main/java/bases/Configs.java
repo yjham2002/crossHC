cfbb82a9c75df68d9f105c2e2c9fb412f3a91075
yjham2002 <yjham2002@gmail.com>
2018-08-03 17:22:20 +0900

    Host changed

--- app/src/main/java/bases/Configs.java
+++ app/src/main/java/bases/Configs.java
@@ -8,7 +8,7 @@ public class Configs {
 
     public static final int APP_ID = 1;
 
-    public static final String BASE_URL = "http://picklecode.co.kr";
+    public static final String BASE_URL = "http://findpictures.cafe24.com";
     public static final String BASE_IMG_POSTFIX = "/uploadFiles/";
     public static final String API_STAGE_DOWN = BASE_URL + "/action_front.php?cmd=ApiList.getStageInfo&appId=" + APP_ID;
     public static final String API_CHECK_UPT = BASE_URL + "/action_front.php?cmd=ApiList.checkUpdate&appId=" + APP_ID;
